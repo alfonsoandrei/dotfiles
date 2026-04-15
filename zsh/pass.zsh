7ee45031e25dcc90e8f0d6c09c451eb7a35283a9
Andrei Alfonso Dubovchenko <alfonsoandrei@gmail.com>
2026-04-15 09:52:30 -0400

    chore: password management
    - pass fzf lookup polish
    - gpg keys caching

--- zsh/pass.zsh
+++ zsh/pass.zsh
@@ -1,9 +1,10 @@
-pass() {
-    git -C ~/.password-store pull --quiet --rebase 2>/dev/null
-    command pass "$@"
-}
+# pass() {
+#     git -C ~/.password-store pull --quiet --rebase 2>/dev/null
+#     command pass "$@"
+# }
 
 alias p='pass'
 alias pc='pass show --clip'
 alias pg='pass generate'
 alias pe='pass edit'
+alias psync='pass git pull && pass git push'
